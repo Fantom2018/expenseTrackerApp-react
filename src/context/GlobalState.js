705f35150763b3d1e40ec6c3f4eee377460e7a8e
Faizulla <fant@zzz.xyz>
2021-09-04 00:12:38 +0500

    End

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,13 +2,14 @@ import React, {createContext, useReducer} from 'react';
 import AppReducer from './AppReducer';
 
 const initialState = {
-   transactions: [
-     { id: 1, text: 'Flower', amount: -20 },
-     { id: 2, text: 'Salary', amount: 300 },
-     { id: 3, text: 'Book', amount: -10 },
-     { id: 4, text: 'Camera', amount: 150 }
-   ]
+   transactions: []
 }
+
+   //  { id: 1, text: 'Flower', amount: -20 },
+   //  { id: 2, text: 'Salary', amount: 300 },
+   //  { id: 3, text: 'Book', amount: -10 },
+   //  { id: 4, text: 'Camera', amount: 150 }
+
 // Create context
 export const GlobalContext = createContext(initialState);
 // Provider component
