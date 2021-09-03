2cfbd97c430c4e4035c7fdc03241ded6e25a78d3
Faizulla <fant@zzz.xyz>
2021-09-03 14:17:34 +0500

    Commit2 , components in progress

---
+++ src/components/Transaction.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Transaction = () => {
+   return (
+      <div>
+         <ul  className="list">
+            <li className="minus">
+                Cash <span>-$400</span><button class="delete-btn">x</button>
+            </li>
+      </ul>
+      </div>
+   )
+}
